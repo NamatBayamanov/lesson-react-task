cf8309a41af11b6abf0ba1828be6b9305e79030b
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-08 01:17:37 +0600

    added node sass, react-icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { BsCart4 } from "react-icons/bs";
 
+BsCart4
 function App() {
   return (
     <div className="App">
-      Hello world
-      
+      <div className="wrapper">
+
+        <div className="Logo">
+          <h2 className="logo__title">
+            REACT PIZZA
+          </h2>
+          <span>
+            it is just words nothing important
+          </span>
+          <img src="#" alt="#" />
+
+        </div>
+
+        <button>
+          $320
+          <span></span>
+          <BsCart4/>
+        </button>
+        <nav>
+          
+        </nav>
+      </div>
     </div>
   );
 }
